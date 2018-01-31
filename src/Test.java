3a4a0bbfda2a339f3999a27508daea2741713b63
Unknown <cs2teacher@gmail.com>
2018-01-31 11:08:56 -0600

    address ok

--- src/Test.java
+++ src/Test.java
@@ -3,8 +3,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Daniel");
 		System.out.println("Hello Osterberg");
+		System.out.println("1234 bob smith");
 	}
 
 }
